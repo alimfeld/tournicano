6890c090fd016d252cce5f8f9119c3f36b251b27
alimfeld <aimfeld@gmx.ch>
2025-06-04 14:08:55 +0200

    feat: revamp tournament

--- main.ts
+++ main.ts
@@ -3,202 +3,259 @@ import {
   weight as maximumMatching,
 } from "@graph-algorithm/maximum-matching";
 
-function assert(condition: boolean, msg?: string): asserts condition {
-  if (!condition) {
-    throw new Error(msg);
-  }
+type Team = [number, number];
+type Match = [Team, Team];
+type Score = [number, number];
+
+export interface Player {
+  readonly name: string;
+  readonly active: boolean;
 }
 
-export class Player {
-  id = crypto.randomUUID();
-  name: string;
-
-  matches = 0;
-  wins = 0;
-  losses = 0;
-  draws = 0;
-  points = 0;
-  pointsAgainst = 0;
-
-  playedWithCounts = new Map<string, number>();
-  playedAgainstCounts = new Map<string, number>();
-
-  constructor(name: string) {
-    this.name = name;
-  }
-
-  playedWithCount(player: Player) {
-    return this.playedWithCounts.get(player.id) || 0;
-  }
-
-  playedAgainstCount(player: Player) {
-    return this.playedAgainstCounts.get(player.id) || 0;
-  }
-
-  playedAgainstSum(players: Player[]) {
-    return players.reduce(
-      (sum, player) => sum + this.playedAgainstCount(player),
-      0,
-    );
-  }
-
-  commitResult(
-    points: number,
-    pointsAgainst: number,
-    partner: Player,
-    opposingTeam: Team,
-  ) {
-    this.matches++;
-    this.points += points;
-    this.pointsAgainst += pointsAgainst;
-
-    if (points > pointsAgainst) this.wins++;
-    else if (points < pointsAgainst) this.losses++;
-    else this.draws++;
-
-    this.playedWithCounts.set(partner.id, this.playedWithCount(partner) + 1);
-    opposingTeam.players.forEach((player) =>
-      this.playedAgainstCounts.set(
-        player.id,
-        this.playedAgainstCount(player) + 1,
-      )
-    );
-  }
+export interface PlayerStats {
+  readonly matches: number;
+  readonly pauses: number;
+  readonly partners: number[];
+  readonly opponents: number[];
+  readonly wins: number;
+  readonly losses: number;
+  readonly draws: number;
+  readonly plus: number; // total points scored
+  readonly minus: number; // total points conceded
 }
 
-export class Team {
-  players;
-
-  constructor(players: [Player, Player]) {
-    this.players = players;
-  }
-
-  playedAgainstSum(team: Team) {
-    return this.players.reduce(
-      (sum, player) => sum + player.playedAgainstSum(team.players),
-      0,
-    );
-  }
-
-  commitResult(result: [number, number], opposingTeam: Team) {
-    this.players.forEach((player, index) => {
-      const partnerIndex = (index + 1) % 2;
-      player.commitResult(
-        result[index],
-        result[partnerIndex],
-        this.players[partnerIndex],
-        opposingTeam,
-      );
-    });
-  }
-}
-
-export class Match {
-  teams;
-  result: [number, number] | undefined;
-
-  constructor(teams: [Team, Team]) {
-    this.teams = teams;
-  }
-
-  commitResult(result: [number, number]) {
-    this.result = result;
-    this.teams.forEach((team, index) =>
-      team.commitResult(result, this.teams[(index + 1) % 2])
-    );
-  }
-}
-
-export class Round {
-  matches: Match[];
-
-  constructor(matches: Match[]) {
-    this.matches = matches;
-  }
-
-  commitResults(results: [number, number][]) {
-    assert(results.length == this.matches.length);
-
-    this.matches.forEach((match, index) => match.commitResult(results[index]));
-  }
+export interface Round {
+  readonly inactive: number[];
+  readonly paused: number[];
+  readonly matches: Match[];
 }
 
 export class Tournament {
-  players: Player[];
-  matchesPerRound: number;
-  completedRounds: Round[] = [];
-  currentRound: Round | null = null;
+  players: Player[] = [];
+  rounds: Round[] = [];
+  scores: Score[][] = [];
+  private stats: DefaultPlayerStats[] = [];
 
-  constructor(playerNames: string[], matchesPerRound: number) {
-    this.players = playerNames.map((name) => new Player(name));
-    this.matchesPerRound = matchesPerRound;
+  enrollPlayers(names: string[]) {
+    names.forEach((name) => {
+      this.players.push({ name, active: true });
+      this.stats.push(new DefaultPlayerStats());
+    });
   }
 
-  generateRound(): Round {
-    const players = this.electPlayers();
-    const teams = this.teamUp(players);
+  updatePlayer(p: number, props: Player) {
+    this.players[p] = { ...props };
+  }
+
+  createRound(matchCount: number): number {
+    const [competing, paused, inactive] = this.partitionPlayers(matchCount);
+    const teams = this.teamUp(competing);
     const matches = this.pairUp(teams);
-    const round = new Round(matches);
-    this.currentRound = round;
+    const round = { inactive, paused, matches };
+    this.submitRound(round);
+    return this.rounds.length - 1;
+  }
+
+  printRound(r: number) {
+    const round = this.getRound(r);
+    const scores = this.scores[r];
+    round.matches.forEach((match, m) => {
+      const p0 = match[0][0];
+      const q0 = match[0][1];
+      const p1 = match[1][0];
+      const q1 = match[1][1];
+      let line = `${this.players[p0].name} & ${this.players[q0].name} vs. ${
+        this.players[p1].name
+      } & ${this.players[q1].name}`;
+      if (scores) {
+        line += ` - ${scores[m][0]} : ${scores[m][1]}`;
+      }
+      console.log(line);
+    });
+  }
+
+  printStats() {
+    this.stats.forEach((stats, p) => {
+      console.log(
+        `${this.players[p].name} R${stats.matches}/${
+          stats.matches + stats.pauses
+        }|W${stats.wins}|D${stats.draws}|L${stats.losses}|Δ${
+          stats.plus - stats.minus
+        }`,
+      );
+    });
+  }
+
+  submitScores(r: number, scores: Score[]) {
+    const round = this.getRound(r);
+    if (round.matches.length != scores.length) {
+      throw Error("Scores mismatch");
+    }
+    round.matches.forEach((match, m) => {
+      const score = scores[m];
+      match[0].forEach((p) => {
+        this.stats[p].submitScore(score[0], score[1]);
+      });
+      match[1].forEach((p) => {
+        this.stats[p].submitScore(score[1], score[0]);
+      });
+    });
+    this.scores.push(scores);
+  }
+
+  getStats(p: number): PlayerStats {
+    return this.stats[p];
+  }
+
+  getAllStats(): PlayerStats[] {
+    return this.stats;
+  }
+
+  private getRound(r: number): Round {
+    const round = this.rounds[r];
+    if (!round) {
+      throw Error("No such round");
+    }
     return round;
   }
 
-  commitResults(results: [number, number][]) {
-    assert(this.currentRound != null);
-
-    this.currentRound.commitResults(results);
-
-    this.completedRounds.push(this.currentRound);
-    this.currentRound = null;
-  }
-
-  private electPlayers(): Player[] {
-    const totalPlayerCount = this.players.length;
-    let playersToElectCount = this.matchesPerRound * 4;
-    if (totalPlayerCount < playersToElectCount) {
-      playersToElectCount = totalPlayerCount - (totalPlayerCount % 4);
-    }
-    if (totalPlayerCount == playersToElectCount) {
-      return this.players;
-    }
-    const electedPlayers = this.players.toSorted((a, b) =>
-      a.matches - b.matches
+  private partitionPlayers(
+    matchCount: number,
+  ): [competing: number[], paused: number[], inactive: number[]] {
+    const [active, inactive] = this.players.reduce(
+      (acc: [number[], number[]], player, p) => {
+        acc[player.active ? 0 : 1].push(p);
+        return acc;
+      },
+      [[], []],
     );
-    return electedPlayers.slice(0, playersToElectCount);
+    let competitorCount = matchCount * 4;
+    if (active.length < competitorCount) {
+      competitorCount = active.length - (active.length % 4);
+    }
+    if (active.length == competitorCount) {
+      return [
+        active,
+        [],
+        inactive,
+      ];
+    }
+    active.sort((p, q) => {
+      // simple sorting based on matches only (i.e. not considering pauses)
+      return this.stats[p].matches - this.stats[q].matches;
+    });
+    return [
+      active.slice(0, competitorCount),
+      active.slice(competitorCount),
+      inactive,
+    ];
   }
 
-  private teamUp(players: Player[]): Team[] {
+  private teamUp(players: number[]): Team[] {
     const edges = [];
     for (let i = 0; i < players.length - 1; i++) {
       for (let j = i + 1; j < players.length; j++) {
+        const p = players[i];
+        const q = players[j];
         edges.push([
           i,
           j,
-          this.completedRounds.length - players[i].playedWithCount(players[j]),
+          // ensure positive weight
+          this.rounds.length - this.stats[p].partnerCount(q),
         ]);
       }
     }
     const matching = maximumMatching(edges);
-    return [...iter(matching)].map((edge: [number, number]) =>
-      new Team([players[edge[0]], players[edge[1]]])
-    );
+    return [...iter(matching)].map((
+      edge: [number, number],
+    ) => [players[edge[0]], players[edge[1]]]);
   }
 
   private pairUp(teams: Team[]): Match[] {
     const edges = [];
     for (let i = 0; i < teams.length - 1; i++) {
       for (let j = i + 1; j < teams.length; j++) {
+        let oppenentSum = 0;
+        teams[i].forEach((p) => {
+          teams[j].forEach((q) => {
+            oppenentSum += this.stats[p].opponentCount(q);
+          });
+        });
         edges.push([
           i,
           j,
-          4 * this.completedRounds.length - teams[i].playedAgainstSum(teams[j]),
+          // ensure positive weight
+          4 * this.rounds.length - oppenentSum,
         ]);
       }
     }
     const matching = maximumMatching(edges);
+    return [...iter(matching)].map((
+      edge: [number, number],
+    ) => [teams[edge[0]], teams[edge[1]]]);
+  }
 
-    return [...iter(matching)].map((edge: [number, number]) =>
-      new Match([teams[edge[0]], teams[edge[1]]])
-    );
+  private submitRound(round: Round) {
+    round.paused.forEach((p) => this.stats[p].pauses++);
+    round.matches.forEach((match) => {
+      match.forEach((t) => {
+        const p = t[0];
+        const q = t[1];
+        this.stats[p].matches++;
+        this.stats[p].incPartner(q);
+        this.stats[q].matches++;
+        this.stats[q].incPartner(p);
+      });
+      match[0].forEach((p) => {
+        match[1].forEach((q) => {
+          this.stats[p].incOppenent(q);
+          this.stats[q].incOppenent(p);
+        });
+      });
+    });
+    this.rounds.push(round);
+  }
+}
+
+class DefaultPlayerStats implements PlayerStats {
+  // updated from rounds
+  matches: number = 0;
+  pauses: number = 0;
+  partners: number[] = [];
+  opponents: number[] = [];
+
+  partnerCount(q: number) {
+    return this.partners[q] || 0;
+  }
+
+  incPartner(q: number) {
+    this.partners[q] = this.partnerCount(q) + 1;
+  }
+
+  opponentCount(q: number) {
+    return this.opponents[q] || 0;
+  }
+
+  incOppenent(q: number) {
+    this.opponents[q] = this.opponentCount(q) + 1;
+  }
+
+  // updated from scores
+  wins: number = 0;
+  losses: number = 0;
+  draws: number = 0;
+  plus: number = 0; // total points scored
+  minus: number = 0; // total points conceded
+
+  submitScore(plus: number, minus: number) {
+    if (plus > minus) {
+      this.wins++;
+    } else if (plus < minus) {
+      this.losses++;
+    } else {
+      this.draws++;
+    }
+    this.plus += plus;
+    this.minus += minus;
   }
 }
--- main_test.ts
+++ main_test.ts
@@ -1,69 +1,109 @@
-import { assertEquals } from "@std/assert";
-import { Player, Round, Tournament } from "./main.ts";
+import { expect } from "@std/expect";
+import { Tournament } from "./main.ts";
 
-const dumpRound = (round: Round) => {
-  for (const match of round.matches) {
-    console.log(
-      `${match.teams[0].players[0].name} & ${
-        match.teams[0].players[1].name
-      } vs. ${match.teams[1].players[0].name} & ${
-        match.teams[1].players[1].name
-      } `,
-    );
-  }
-};
+const players = [
+  "Ace",
+  "Ben",
+  "Cam",
+  "Dan",
+  "Eli",
+  "Fay",
+  "Gus",
+  "Hal",
+  "Ivy",
+  "Jay",
+];
 
-const dumpPlayers = (players: Player[]) => {
-  for (const player of players) {
-    console.log(
-      `${player.name} M${player.matches}/W${player.wins}/L${player.losses}/D${player.draws}/Δ${
-        player.points - player.pointsAgainst
-      }`,
-    );
-  }
-};
+const allScores: [number, number][][] = [
+  [[11, 3], [8, 11]],
+  [[11, 7], [11, 2]],
+  [[8, 8], [1, 11]],
+  [[11, 0], [6, 6]],
+  [[11, 9], [11, 4]],
+];
 
-Deno.test(function runTournament() {
-  const tournament = new Tournament([
-    "Ali",
-    "Ben",
-    "Cam",
-    "Dan",
-    "Eli",
-    "Fay",
-    "Gus",
-    "Hal",
-    "Ivy",
-    "Jay",
-  ], 2);
-
-  const roundResults: [number, number][][] = [
-    [[11, 3], [8, 11]],
-    [[11, 7], [11, 2]],
-    [[8, 8], [1, 11]],
-    [[11, 0], [6, 6]],
-    [[11, 9], [11, 4]],
-    [[11, 7], [2, 11]],
-    [[11, 7], [11, 2]],
-    [[8, 8], [1, 11]],
-    [[11, 0], [6, 6]],
-    [[11, 9], [11, 4]],
-  ];
-
-  roundResults.forEach((results, index) => {
-    assertEquals(tournament.completedRounds.length, index);
-    assertEquals(tournament.currentRound, null);
-    const round = tournament.generateRound();
-    console.log("-------");
-    console.log(`Round ${index + 1}`);
-    console.log("-------");
-    dumpRound(round);
-    console.log("-------");
-    assertEquals(tournament.currentRound, round);
-    assertEquals(round.matches.length, 2);
-    tournament.commitResults(results);
-    dumpPlayers(tournament.players);
-    assertEquals(tournament.completedRounds.length, index + 1);
-    assertEquals(tournament.currentRound, null);
+const runTournament = () => {
+  const tournament = new Tournament();
+  tournament.enrollPlayers(players);
+  allScores.forEach((scores) => {
+    const r = tournament.createRound(scores.length);
+    tournament.submitScores(r, scores);
+    console.log(`Round ${r + 1}:`);
+    tournament.printRound(r);
   });
+  console.log("Stats:");
+  tournament.printStats();
+  return tournament;
+};
+
+const tournament = runTournament();
+
+Deno.test("should store rounds", () => {
+  expect(tournament.rounds.length).toBe(allScores.length);
+});
+
+Deno.test("should update stats from pairings", () => {
+  tournament.getAllStats().forEach((stats) => {
+    expect(stats.partners.reduce((acc, count) => acc + count, 0)).toBe(4);
+    expect(stats.opponents.reduce((acc, count) => acc + count, 0)).toBe(8);
+  });
+});
+
+Deno.test("should balance play percentage", () => {
+  tournament.getAllStats().forEach((stats) => {
+    expect(stats.matches).toBe(4);
+    expect(stats.pauses).toBe(1);
+  });
+});
+
+Deno.test("should balance partners", () => {
+  tournament.getAllStats().forEach((stats) => {
+    stats.partners.forEach((count) => {
+      expect(count).toBeLessThanOrEqual(1);
+    });
+  });
+});
+
+Deno.test("should balance opponents", () => {
+  tournament.getAllStats().forEach((stats) => {
+    stats.opponents.forEach((count) => {
+      expect(count).toBeLessThanOrEqual(2);
+    });
+  });
+});
+
+Deno.test("should store scores", () => {
+  expect(tournament.scores.length).toBe(5);
+});
+
+Deno.test("should distribute scores", () => {
+  const scoreSum = allScores.reduce(
+    (acc, scores) =>
+      acc + scores.reduce((acc, score) => acc + score[0] + score[1], 0),
+    0,
+  );
+  const [totalPlus, totalMinus] = tournament.getAllStats().reduce(
+    (acc, stats) => [acc[0] + stats.plus, acc[1] + stats.minus],
+    [0, 0],
+  );
+  expect(totalPlus).toBe(scoreSum * 2);
+  expect(totalMinus).toBe(totalPlus);
+});
+
+Deno.test("should distribute results", () => {
+  const decisive = allScores.reduce(
+    (acc, scores) =>
+      acc +
+      scores.reduce((acc, score) => acc + (score[0] == score[1] ? 0 : 1), 0),
+    0,
+  );
+  const [wins, losses, draws] = tournament.getAllStats().reduce(
+    (
+      acc,
+      stats,
+    ) => [acc[0] + stats.wins, acc[1] + stats.losses, acc[2] + stats.draws],
+    [0, 0, 0],
+  );
+  expect(wins + losses).toBe(decisive * 4);
+  expect(draws).toBe((10 - decisive) * 4);
 });
