17df4ca11074832d39b8f8d04de7f56dab2d1f0b
Alexander Imfeld <alexander.imfeld@netcetera.com>
2025-06-15 16:16:56 +0200

    feat: add vite base

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  base: "/tournicano/",
   resolve: {
     alias: {
-      "@graph-algorithm/maximum-matching": "./node_modules/@graph-algorithm/maximum-matching/dist/module/index.js"
-    }
+      "@graph-algorithm/maximum-matching":
+        "./node_modules/@graph-algorithm/maximum-matching/dist/module/index.js",
+    },
   },
   optimizeDeps: {
-    include: ["@graph-algorithm/maximum-matching"]
+    include: ["@graph-algorithm/maximum-matching"],
   },
   test: {
     alias: {
-      "@graph-algorithm/maximum-matching": "@graph-algorithm/maximum-matching"
-    }
-  }
-})
+      "@graph-algorithm/maximum-matching": "@graph-algorithm/maximum-matching",
+    },
+  },
+});
