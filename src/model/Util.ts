242645f1864fdb5f6af08c79920a91583288ddb7
Alexander Imfeld <>
2025-11-23 16:02:29 +0100

    feat: make shuffle generic

--- src/model/Util.ts
+++ src/model/Util.ts
@@ -1,6 +1,6 @@
 // Fisher-Yates shuffle
-export const shuffle = (array: any[]) => {
-  const result = [...array]
+export const shuffle = <T>(array: T[]): T[] => {
+  const result = [...array];
   for (let i = result.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [result[i], result[j]] = [result[j], result[i]];
